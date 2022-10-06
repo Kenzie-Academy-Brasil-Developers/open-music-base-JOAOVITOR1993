61b1c057a2b5f59a05239fb4fed520ce70ad48a3
joaovitor <carrera93jv@gmail.com>
2022-10-06 16:39:17 -0300

    feat: ajuste bug darkmode

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -1,22 +1,27 @@
+const html = document.querySelector("html")
+const preferenciaDark = localStorage.getItem('darkmode')
+const imgDark = document.querySelector("#imgDark")
+if(preferenciaDark){
+     html.classList.add("dark-mode")
+     imgDark.src = "./assets/img/sun.png"
+ }
+ 
 function darkMode(){
-    const html = document.querySelector("html")
     const botaoDark = document.querySelector("#darkMode")
-    const imgDark = document.querySelector("#imgDark")
-    const preferenciaDark = localStorage.getItem('darkmode')
-
-    if(preferenciaDark){
-        html.classList.add("dark-mode")
-        imgDark.src = "./assets/img/sun.png"
-    }
 
     botaoDark.addEventListener("click", ()=>{
         html.classList.toggle("dark-mode")
-
-        if(!preferenciaDark){
+        
+        const preferencia = localStorage.getItem('darkmode')
+        if(!preferencia){
             localStorage.setItem('darkmode', true)
+            console.log(preferencia)
         }else{
             localStorage.removeItem('darkmode')
+            console.log(preferencia)
         }
+        console.log("html",html)
+        
 
         if(!html.classList.contains("dark-mode")){
             imgDark.src = "./assets/img/moon.png"
